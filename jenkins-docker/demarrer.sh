24794efe97eb6d3ac3726ab2cf4a16c681875778
root <root@localhost.localdomain>
2020-05-29 19:12:07 +0000

    jenkinsfile

--- jenkins-docker/demarrer.sh
+++ jenkins-docker/demarrer.sh
@@ -1,1 +1,1 @@
-docker run -d -u root -p 8080:8080 -p 50000:50000 -v /var/run/docker.sock:/var/run/docker.sock -v /home/vagrant/jenkins_home:/var/jenkins_home:rw -v /bin/docker:/bin/docker ldomingo/jenkins
+docker run -d -u root -p 8080:8080 -p 50000:50000 -v /var/run/docker.sock:/var/run/docker.sock -v /home/vagrant/projet-minikube/jenkins-docker/jenkins_home:/var/jenkins_home:rw -v /bin/docker:/bin/docker ldomingo/jenkins
